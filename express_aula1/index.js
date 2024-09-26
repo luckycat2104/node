769a1112f49edcf41ac0e6f247ec3dd531963310
lucca <laramsonza@gmail.com>
2024-09-26 09:56:15 -0300

    aaa

--- express_aula1/index.js
+++ express_aula1/index.js
@@ -4,6 +4,15 @@ const port  = 3000 //variavel ambiente
 
 const path = require("path")
 const basepath = path.join(__dirname, 'Aula1')
+const checkAuth = function(req, res, next){
+    req.authStatus = true
+    if (req.authStatus){
+        console.log ('Está logado, pode continuar!')
+    } else {
+        console.log ('Não está logado, faça o login')
+    }
+}
+app.use(checkAuth)
 
 app.get('/', (req, res) => {
     res.sendFile(`${basepath}/index.html`)
